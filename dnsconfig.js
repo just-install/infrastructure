afbb2461d72c8a3edbcaff5dbc241d4d1962c8ab
Patrick G <geek1011@outlook.com>
2018-04-11 17:36:36 -0400

    Fix CNAME entries

--- dnsconfig.js
+++ dnsconfig.js
@@ -7,7 +7,7 @@ D("just-install.it", REG_OVH,
     A("@","104.198.14.52"),
     A("go","213.186.33.5"),
     TXT("go","1|https://stable.just-install.it"),
-    CNAME("registry","just-install-registry.netlify.com"),
-    CNAME("stable","just-install-stable.netlify.com"),
-    CNAME("unstable","just-install-unstable.netlify.com")
+    CNAME("registry","just-install-registry.netlify.com."),
+    CNAME("stable","just-install-stable.netlify.com."),
+    CNAME("unstable","just-install-unstable.netlify.com.")
 );
