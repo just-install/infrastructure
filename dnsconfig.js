94067dd9bfca761931d693b56ab2de0c64a91e48
Patrick G <geek1011@outlook.com>
2018-04-16 16:02:23 -0400

    Fix provider name

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,5 +1,5 @@
 var OVH = NewDnsProvider('ovh','OVH');
-var CLOUDFLARE = NewRegistrar('cloudflare', 'CLOUDFLARE');
+var CLOUDFLARE = NewRegistrar('cloudflare', 'CLOUDFLAREAPI');
 var REG_NONE = NewRegistrar('none', 'NONE');
 
 D("just-install.it", REG_NONE,
