c452a9907b964af0516c11cac6a662f7a96c80e4
Patrick G <geek1011@outlook.com>
2018-04-16 15:55:38 -0400

    CloudFlare DNS

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,13 +1,16 @@
 var OVH = NewDnsProvider('ovh','OVH');
-var REG_OVH = NewRegistrar('ovh', 'OVH');
+var CLOUDFLARE = NewRegistrar('cloudflare', 'CLOUDFLARE');
+var REG_NONE = NewRegistrar('none', 'NONE');
 
-D("just-install.it", REG_OVH,
-    DnsProvider(OVH),
+D("just-install.it", REG_NONE,
+    DnsProvider(CLOUDFLARE, 0),
     DefaultTTL(3599),
+    CF_PROXY_DEFAULT_OFF,
     A("@","104.198.14.52"),
     A("go","213.186.33.5"),
     TXT("go","1|https://stable.just-install.it"),
     CNAME("registry","just-install-registry.netlify.com."),
     CNAME("stable","just-install-stable.netlify.com."),
-    CNAME("unstable","just-install-unstable.netlify.com.")
+    CNAME("unstable","just-install-unstable.netlify.com."),
+    { no_ns: "true" }
 );
